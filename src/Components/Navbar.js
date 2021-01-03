861a2e6c2a78306fefb986d14a5ed4417b86a31e
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-01-02 22:00:53 -0800

    test

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,2 +1,25 @@
 import React from 'react';
-import {Navbar, Nav}
+import {Navbar, Nav } from 'react-bootstrap';
+import { styled } from 'styled-components';
+
+const Styles = styled.div`
+.navbar {
+   back-color: #222;
+}
+
+.navbar-brand, .navbar-nav .nav-link {
+   color: #bbb;
+
+   &:hover {
+      color: white;
+   }
+}
+`;
+
+export const Navbar = () => {
+   <Styles>
+      <Navbar expand="lg">
+         <Navbar.Brand href="/">Code Life</Navbar.Brand>
+         </Navbar>
+   </Styles>
+}
