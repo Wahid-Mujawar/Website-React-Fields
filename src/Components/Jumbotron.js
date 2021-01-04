fc3fc25cd156150ea22b67b51de8f3b4ca238e5e
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-01-03 19:10:10 -0800

    test

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
-// import CristianoRonaldo1 from '../Components/CristianoRonaldo1.jpg';
+import CristianoRonaldo1 from '../Components/CristianoRonaldo1.jpg';
 
 
 const Styles = styled.div`
