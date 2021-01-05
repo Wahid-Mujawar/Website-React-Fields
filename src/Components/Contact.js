a2888d86e7c2f7a3111cc15263e07995b7ee73e8
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-01-04 22:26:21 -0800

    test

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,12 +4,12 @@ export const Contact = () => (
    <div>
       <h2>Contact Me</h2>
 					   
-         AbdulWahid Mujawar<br/>
-         Masajid Galli Halasi (591120)< br /> 
-         Tq-Khanapur
-         Dist-Belagavi <br />
-         Belagavi  Karnataka (590001)<br />
-         8088686729
+         {/* AbdulWahid Mujawar<br/> */}
+         {/* Masajid Galli Halasi (591120)< br />  */}
+         {/* Tq-Khanapur */}
+         {/* Dist-Belagavi <br /> */}
+         {/* Belagavi  Karnataka (590001)<br /> */}
+         {/* 8088686729 */}
 			
    </div>
 )
